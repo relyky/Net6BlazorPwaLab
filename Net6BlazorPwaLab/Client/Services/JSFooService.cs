77e2e2e9285529fc79aab5a59d1572063bc66979
Rely Ky <relyky@hotmail.com>
2022-03-29 15:47:38 +0800

    JS FooModule 測試

---
+++ Net6BlazorPwaLab/Client/Services/JSFooService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Net6BlazorPwaLab.Client.Services;
+
+public class JSFooService
+{
+  //# Injection Member
+  readonly IJSRuntime jsr;
+
+  //# resource
+  IJSObjectReference jsModule;
+
+  //#
+  IJSObjectReference bar1, bar2;
+
+  public JSFooService(IJSRuntime _jsr)
+  {
+    jsr = _jsr;
+  }
+
+  public async Task ImportAsync()
+  {
+    jsModule = await jsr.InvokeAsync<IJSObjectReference>("import", "./js/FooModule.js");
+    bar1 = await jsModule.InvokeAsync<IJSObjectReference>("Bar", "barA");
+    bar2 = await jsModule.InvokeAsync<IJSObjectReference>("Bar", "barB");
+  }
+
+  public async Task CallActionAsync()
+  {
+    string str3 = await bar1.InvokeAsync<string>("toString");
+    string str4 = await bar2.InvokeAsync<string>("toString");
+
+    await Task.Delay(100);
+  }
+
+}
---
+++ Net6BlazorPwaLab/Client/wwwroot/js/FooModule.js
@@ -0,0 +1,16 @@
+﻿
+function Bar(_name)
+{
+  const name = _name;
+
+  const toString = () => {
+    return `I am Bar:${name}.`;
+  }
+
+  return {
+    name,
+    toString
+  };
+}
+
+export { Bar };
