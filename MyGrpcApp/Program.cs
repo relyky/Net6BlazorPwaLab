bcd58cb794bb50e0f5ab503005625a4a120afbc5
Rely Ky <relyky@hotmail.com>
2022-04-25 18:21:44 +0800

    Grpc 之 HelloWorld

---
+++ MyGrpcApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using MyGrpcService;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! MyGrpcApp. ");
+Console.WriteLine("Press any to go.");
+Console.ReadKey();
+
+Console.WriteLine(">>>>>> GO");
+
+//using var channel = GrpcChannel.ForAddress("http://localhost:5027");
+using var channel = GrpcChannel.ForAddress("https://localhost:7027");
+var client = new Greeter.GreeterClient(channel);
+
+var result = client.SayHello(new HelloRequest { Name = "Foo" });
+
+Console.WriteLine($"result: {result.Message}");
+
+Console.WriteLine(">>>>>> EOF");
