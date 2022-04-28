5a40eead0f7e5cfe7e38885988246da1b840934f
Rely Ky <relyky@hotmail.com>
2022-04-28 12:45:53 +0800

    主機端串流 gRPC

--- MyGrpcApp/Program.cs
+++ MyGrpcApp/Program.cs
@@ -1,19 +1,46 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using MyGrpcService;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! MyGrpcApp. ");
-Console.WriteLine("Press any to go.");
-Console.ReadKey();
-
-Console.WriteLine(">>>>>> GO");
+Console.WriteLine("§§ gRPC Lab");
 
 //using var channel = GrpcChannel.ForAddress("http://localhost:5027");
 using var channel = GrpcChannel.ForAddress("https://localhost:7027");
 var client = new Greeter.GreeterClient(channel);
+Console.WriteLine("已建立 gRPC 連線");
 
-var result = client.SayHello(new HelloRequest { Name = "Foo" });
+while (true)
+{
+  Console.WriteLine(Environment.NewLine + "測試情境指令");
+  Console.WriteLine("[A] 基本通訊");
+  Console.WriteLine("[B] 主機端串流 gRPC (server-side streaming gRPC)");
+  Console.WriteLine("[Esc] 離開");
 
-Console.WriteLine($"result: {result.Message}");
+  var key = Console.ReadKey();
 
-Console.WriteLine(">>>>>> EOF");
+  if (key.Key == ConsoleKey.A)
+  {
+    Console.WriteLine(Environment.NewLine + "# 基本通訊 ------------");
+    var result = client.SayHello(new HelloRequest { Name = "Foo" });
+    Console.WriteLine($"result: {result.Message}");
+  }
+  else if (key.Key == ConsoleKey.B)
+  {
+    Console.WriteLine(Environment.NewLine + "# 主機端串流 gRPC ------------");
+
+    using var resp = client.DownloadCv(new DownloadByName { Name = "Bar" });
+    while (resp.ResponseStream.MoveNext().Result)
+    {
+      var item = resp.ResponseStream.Current;
+      Console.WriteLine($"{item.Name}, {item.Jobs[0].Title}, {item.Jobs[1].Title}");
+    }
+
+    //===========================================
+  }
+  else if (key.Key == ConsoleKey.Escape)
+  {
+    Console.WriteLine("Exit >>>>>>");
+    break;
+  }
+}
--- MyGrpcService/Services/GreeterService.cs
+++ MyGrpcService/Services/GreeterService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Collections;
 using Grpc.Core;
 using MyGrpcService;
 
@@ -17,6 +18,42 @@ namespace MyGrpcService.Services
       {
         Message = "Hello " + request.Name
       });
+    }
+
+    /// <summary>
+    /// 測試：主機端串流 RPC (server-side streaming RPC)
+    /// </summary>
+    public override async Task DownloadCv(DownloadByName request
+      , IServerStreamWriter<Candidate> responseStream
+      , ServerCallContext context)
+    {
+      // 模擬
+      RepeatedField<Job> jobList = new();
+      jobList.Add(new Job { Title = "註冊護士", Salary = 17859, JobDescription = "第一位" });
+      jobList.Add(new Job { Title = "中學教師", Salary = 8716, JobDescription = "第二位" });
+      jobList.Add(new Job { Title = "軟件應用程式工程師", Salary = 8405, JobDescription = "第三位" });
+      jobList.Add(new Job { Title = "電工", Salary = 8021, JobDescription = "第四位" });
+      jobList.Add(new Job { Title = "施工經理", Salary = 7145, JobDescription = "第五位" });
+      jobList.Add(new Job { Title = "木工工匠", Salary = 6812, JobDescription = "第六位" });
+      jobList.Add(new Job { Title = "金屬鉗工和機械技工", Salary = 6335, JobDescription = "第七位" });
+      jobList.Add(new Job { Title = "水喉技工", Salary = 5861, JobDescription = "第八位" });
+      jobList.Add(new Job { Title = "汽車維修技工", Salary = 5292, JobDescription = "第九位" });
+      jobList.Add(new Job { Title = "大學講師", Salary = 5042, JobDescription = "第十位" });
+
+      // 將每筆資料逐一透過 WriteAsync 輸出
+      Candidate new1 = new() { Name = $"{request.Name}#1" };
+      new1.Jobs.AddRange(jobList);
+      await responseStream.WriteAsync(new1);
+
+      // 將每筆資料逐一透過 WriteAsync 輸出
+      Candidate new2 = new() { Name = $"{request.Name}#2" };
+      new2.Jobs.AddRange(jobList);
+      await responseStream.WriteAsync(new2);
+
+      // 將每筆資料逐一透過 WriteAsync 輸出
+      Candidate new3 = new() { Name = $"{request.Name}#3" };
+      new3.Jobs.AddRange(jobList);
+      await responseStream.WriteAsync(new3);
     }
   }
 }
