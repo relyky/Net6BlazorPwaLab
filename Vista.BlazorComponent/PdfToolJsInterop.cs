abca28eb62e3268c0fe9c7915dacfacd6669fe56
Rely Ky <relyky@hotmail.com>
2022-04-29 11:28:18 +0800

    pdfJs hello

--- Vista.BlazorComponent/PdfToolJsInterop.cs
+++ Vista.BlazorComponent/PdfToolJsInterop.cs
@@ -1,37 +1,42 @@
 using Microsoft.JSInterop;
 
-namespace Vista.BlazorComponent
+namespace Vista.BlazorComponent;
+
+// This class provides an example of how JavaScript functionality can be wrapped
+// in a .NET class for easy consumption. The associated JavaScript module is
+// loaded on demand when first needed.
+//
+// This class can be registered as scoped DI service and then injected into Blazor
+// components for use.
+
+public class PdfToolJsInterop : IAsyncDisposable
 {
-  // This class provides an example of how JavaScript functionality can be wrapped
-  // in a .NET class for easy consumption. The associated JavaScript module is
-  // loaded on demand when first needed.
-  //
-  // This class can be registered as scoped DI service and then injected into Blazor
-  // components for use.
+  private readonly Lazy<Task<IJSObjectReference>> moduleTask;
 
-  public class PdfToolJsInterop : IAsyncDisposable
+  public PdfToolJsInterop(IJSRuntime jsRuntime)
   {
-    private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+    moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+       "import", "./_content/Vista.BlazorComponent/pdfToolJsInterop.js").AsTask());
+  }
 
-    public PdfToolJsInterop(IJSRuntime jsRuntime)
-    {
-      moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
-         "import", "./_content/Vista.BlazorComponent/pdfToolJsInterop.js").AsTask());
-    }
+  public async ValueTask<string> Prompt(string message)
+  {
+    var module = await moduleTask.Value;
+    return await module.InvokeAsync<string>("showPrompt", message);
+  }
 
-    public async ValueTask<string> Prompt(string message)
+  public async Task InitAsync()
+  {
+    var module = await moduleTask.Value;
+    await module.InvokeVoidAsync("init");
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+    if (moduleTask.IsValueCreated)
     {
       var module = await moduleTask.Value;
-      return await module.InvokeAsync<string>("showPrompt", message);
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-      if (moduleTask.IsValueCreated)
-      {
-        var module = await moduleTask.Value;
-        await module.DisposeAsync();
-      }
+      await module.DisposeAsync();
     }
   }
 }
